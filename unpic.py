be6959b0d001911747709680f6f11296711969ff
Jay Vakil <61257499+Jdvakil@users.noreply.github.com>
2022-06-01 17:03:45 -0400

    Path checker function

--- unpic.py
+++ unpic.py
@@ -5,4 +5,10 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--file-name", help="file name to be unpickled")
 args = parser.parse_args()
-print(f"Config: {args}")
+cwd = os.getcwd()
+path = os.path.join(cwd, args.file_name)
+
+if (os.path.exists(path)):
+    print(path)
+else:
+    print("Path not found...")
