4c76124891f14d0c89a25121531fb7d1101a69c4
Jay Vakil <61257499+Jdvakil@users.noreply.github.com>
2022-06-01 17:25:26 -0400

    Update unpic.py

--- unpic.py
+++ unpic.py
@@ -4,11 +4,15 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--file-name", help="file name to be unpickled")
+parser.add_argument("--path", default=os.getcwd(), help="Path of the file without the filename")
 args = parser.parse_args()
-cwd = os.getcwd()
+cwd = args.path
 path = os.path.join(cwd, args.file_name)
 
-if (os.path.exists(path)):
-    print(path)
+if (os.path.exists(path)): 
+    print(f"Path - {path}\n")
+    with open(path, 'rb') as f:
+        print(pickle.load(f))
+
 else:
     print("Path not found...")
