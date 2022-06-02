f84d50f2e5066dd3ebec0fa2065e2f3b4881a195
Jay Vakil <61257499+Jdvakil@users.noreply.github.com>
2022-06-02 14:55:07 -0400

    Update unpic.py

--- unpic.py
+++ unpic.py
@@ -1,12 +1,14 @@
 import pickle 
 import os
 
-def unpickle(path):
-    obj = None
-    if(os.path.exists(path)):
-        print(f"Path found - {path}, unpickling...\n")
-        with open(path, 'rb') as f:
-            obj = pickle.load(f)
-    else:
-        obj = f"Path - {path} not found..."
-    return obj
+if __name__ == "__main__":
+
+    def unpickle(path):
+        obj = None
+        if(os.path.exists(path)):
+            print(f"Path found - {path}, unpickling...\n")
+            with open(path, 'rb') as f:
+                obj = pickle.load(f)
+        else:
+            obj = f"Path - {path} not found..."
+        return obj
